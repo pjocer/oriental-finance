1c9a3e8c12ce9a23c93cce83167b83b3e955c870
jigh@oriental-finance.com <jigh@oriental-finance.com>
2017-05-26 18:16:22 +0800

    add enviroment macro & vender macro

---
+++ OFProjectTemplate/OFProjectTemplate/Macro/OEnviroment.h
@@ -0,0 +1,17 @@
+//
+//  OEnviroment.h
+//  OFProjectTemplate
+//
+//  Created by 吉冠虎 on 2017/5/26.
+//  Copyright © 2017年 com.oriental-finance.ios. All rights reserved.
+//
+
+#ifndef OEnviroment_h
+#define OEnviroment_h
+
+
+//#define OF_DEVELOP_ENVIRONMENT         // 开发环境
+
+#define OF_DISTRIBUTION_ENVIRONMENT    // 线上
+
+#endif /* OEnviroment_h */
