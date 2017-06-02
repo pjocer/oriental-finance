d1ac6ac087a398a6f605810cf8dc116b35dfa9ad
jocer <jigh@oriental-finance.com>
2017-06-02 11:41:34 +0800

    1.Pod add FMDB 2.Add OFDabaBase profile

---
+++ OFProjectTemplate/OFProjectTemplate/OFFrameworks/OFDataBase.h
@@ -0,0 +1,13 @@
+//
+//  OFDataBase.h
+//  OFProjectTemplate
+//
+//  Created by 吉冠虎 on 2017/6/2.
+//  Copyright © 2017年 com.oriental-finance.ios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OFDataBase : NSObject
+
+@end
